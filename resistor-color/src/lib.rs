feb43d373ba3388d13d324aa5287b7b3189ac374
zoey <zoey@pop-os.localdomain>
2023-02-21 16:11:24 -0500

    got color_to_value function working

--- resistor-color/src/lib.rs
+++ resistor-color/src/lib.rs
@@ -1,19 +1,22 @@
-#[derive(Debug, PartialEq, Eq)]
+use int_enum::IntEnum;
+
+#[repr(u32)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, IntEnum)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Blue = 6,
+    Brown = 1,
+    Green = 5,
+    Grey = 8,
+    Orange = 3,
+    Red = 2,
+    Violet = 7,
+    White = 9,
+    Yellow = 4,
 }
 
 pub fn color_to_value(color: ResistorColor) -> u32 {
-    unimplemented!("convert color {color:?} into a numerical representation")
+    return color.int_value();
 }
 
 pub fn value_to_color_string(value: u32) -> String {
