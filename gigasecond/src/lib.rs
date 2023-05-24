0304fb4c45ee248f67a1c6ff5b2d5b9a16337ac4
zoey <z@zoey.ninja>
2023-05-23 21:20:04 -0500

    added appropriate duration

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,7 @@
+use time::Duration;
 use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {start}");
+    return start + Duration::seconds(1000000000);
 }
